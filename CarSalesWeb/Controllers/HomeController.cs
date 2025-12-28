22d5fa6a86a908474350f83dd00c5e00566a2bde
Dinura Warakapola <dinurawar@gmail.com>
2025-12-28 17:54:56 +1100

    Finished creating class and created/added 3 objects to list to print

--- CarSalesWeb/Controllers/HomeController.cs
+++ CarSalesWeb/Controllers/HomeController.cs
@@ -6,9 +6,59 @@ namespace CarSalesWeb.Controllers
 {
     public class HomeController : Controller
     {
+
+        //So this i where i created the objects for the cars to be displayed on the index page.
         public IActionResult Index()
         {
-            return View();
+
+            
+
+            Car car1 = new Car
+            {
+
+                CarID = 1,
+                Manufacturer = "Nissan",
+                Model = "370z",
+                Year = 2010,
+                Price = 20000,
+                Odometer = 100000,
+                Category = "sports",
+                Image = "image"
+            };
+
+            Car car2 = new Car
+            {
+
+                CarID = 2,
+                Manufacturer = "Mitsubishi",
+                Model = "Evolution V",
+                Year = 1999,
+                Price = 60000,
+                Odometer = 98000,
+                Category = "sports rally",
+                Image = "image"
+            };
+
+            Car car3 = new Car
+            {
+
+                CarID = 3,
+                Manufacturer = "Subaru",
+                Model = "Impreza WRX STI",
+                Year = 2001,
+                Price = 80000,
+                Odometer = 50000,
+                Category = "sports Rally",
+                Image = "image"
+            };
+
+            var Carlist = new List<Car> { };
+            Carlist.Add(car1);
+            Carlist.Add(car2);
+            Carlist.Add(car3);
+
+            return View(Carlist);
+
         }
 
         public IActionResult Privacy()
---
+++ CarSalesWeb/Models/carproperties.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CarSalesWeb.Models
+{
+    public class carproperties
+    {
+
+
+
+
+
+    }
+    //So pretty much i just created a class with attributes that have constructors.
+    //The actual objects are created in HomeController.cs so check that out.
+    public class Car
+    {
+        public int CarID { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Price { get; set; }
+        public int Odometer { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+    }
+}
